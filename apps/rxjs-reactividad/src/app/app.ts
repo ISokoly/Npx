97d4a7bcdff68d6c80c08310b1aaab39fa9e1002
Aronperalta <elaron_2009@hotmail.com>
2025-09-25 19:03:37 -0500

    readonly

--- apps/rxjs-reactividad/src/app/app.ts
+++ apps/rxjs-reactividad/src/app/app.ts
@@ -1,31 +1,26 @@
 import { RouterModule } from '@angular/router';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
+import { interval } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
-  contador = 0;
+  readonly contador$ = interval(1000);
 
-  readonly changeDetection = inject(ChangeDetectorRef)
+  //readonly changeDetection = inject(ChangeDetectorRef)
 
   metodoCambiar() {
-
+    console.log("Cambiando")
+    return 55
   }
 
-  mensaje(){
-    console.log("Hola")
-    return 45;
-  }
-  
-  constructor() {
-    setInterval(() => {
-      this.contador++
-      console.log(this.contador);
-    }, 1000);
-  }
+  //mensaje() {console.log("Hola")return 45;}
+
+  //constructor() { setInterval(() => { this.contador++ console.log(this.contador);}, 1000);}
 }
